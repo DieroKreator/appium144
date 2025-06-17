39818ae8d016a48b84f42a48af07edb00b07e7bc
DieroKreator <qtechwired@gmail.com>
2025-06-16 21:27:42 -0600

    Rename test function from test_comprar_camiseta to test_comprar_mochila_verm for clarity

--- tests/test_comprar_mochila_verm.py
+++ tests/test_comprar_mochila_verm.py
@@ -6,7 +6,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_comprar_camiseta():
+def test_comprar_mochila_verm():
 
     options = AppiumOptions()
     options.load_capabilities({
